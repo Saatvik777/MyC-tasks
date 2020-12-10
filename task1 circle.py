d3f3bfa43ec31b3ae8b3581e2d4f75333caac668
Saatvik777 <68275667+Saatvik777@users.noreply.github.com>
2020-12-10 15:05:44 +0530

    task1

---
+++ task1 circle.py
@@ -0,0 +1,3 @@
+r = float(input("input the radius of the circle"));
+a = 3.14*r*r;
+print("the area of the circle with radius %.2f" %r ,'is:%0.4f' %a);
---
+++ task1 file ext.py
@@ -0,0 +1,3 @@
+filename = input("Input the Filename:")
+f_extension = filename.split(".")
+print ("The extension of the file is:" + (f_extension[-1]))
